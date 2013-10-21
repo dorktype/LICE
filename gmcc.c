3c3916c2181783a2743bc31da9ab0349cb2f7750
Dale Weiler <killfieldengine@gmail.com>
2013-10-21 05:39:35 -0400

    Implemented support for string literals.

--- gmcc.c
+++ gmcc.c
@@ -1,43 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
-#define GMCC_ENTRY     "gmcc_main"
-#define GMCC_ASSEMBLER "as"
-#define GMCC_LINKER    "gcc"
+#define GMCC_ENTRY_INT "gmcc_entry_int"
+#define GMCC_ENTRY_STR "gmcc_entry_str"
+#define GMCC_ASSEMBLER "as -o blob.o"
+#define GMCC_LINKER    "gcc blob.o invoke.c -o program"
 
-static void generate(int input) {
-    FILE *as;
+static void compile_error(const char *fmt, ...) {
+    va_list  a;
+    va_start(a, fmt);
+    vfprintf(stderr, fmt, a);
+    fprintf(stderr, "\n");
+    va_end(a);
 
-    if (!(as = popen("as -o blob.o", "w")))
-        abort();
-
-    fprintf(as,
-
-".text\n\
-.global %s\n\
-%s:\n\
-    mov $%d, %%eax\n\
-    ret\n",
-
-        GMCC_ENTRY,
-        GMCC_ENTRY,
-        input
-    );
-
-    fclose(as);
+    exit(EXIT_FAILURE);
 }
 
-static void link(void) {
-    system("gcc blob.o invoke.c -o output");
+static void compile_integer(FILE *as, int value) {
+    int c;
+    while ((c = getc(stdin)) != EOF) {
+        if (isspace(c))
+            break;
+
+        if (!isdigit(c))
+            compile_error("Expected numeric, got `%c` instead", c);
+
+        value = value * 10 + (c - '0');
+    }
+
+    fprintf(
+        as,
+        ".text\n\
+        .global %s\n\
+        %s:\n\
+            mov $%d, %%rax\n\
+            ret\n",
+
+        GMCC_ENTRY_INT,
+        GMCC_ENTRY_INT,
+        value
+    );
+}
+
+static void compile_string(FILE *as) {
+    char buffer[1024];
+    int  i = 0;
+    int  c = EOF;
+
+    for (;;) {
+        if ((c = getc(stdin)) == EOF)
+            compile_error("Unterminated string");
+        if (c == '"')
+            break;
+
+        if (c == '\\') {
+            if ((c = getc(stdin)) == EOF)
+                compile_error("Unterminated line continuation `\\` in string");
+        }
+
+        buffer[i++] = c;
+        if (i == sizeof(buffer) - 1)
+            compile_error("Internal error: OOM");
+
+    }
+
+    buffer[i] = '\0';
+
+    fprintf(
+        as,
+        ".data\n\
+        .gmcc_data:\n\
+        .string \"%s\"\n\
+        .text\n\
+        .global %s\n\
+        %s:\n\
+            lea .gmcc_data(%%rip), %%rax\n\
+            ret\n",
+        buffer,
+        GMCC_ENTRY_STR,
+        GMCC_ENTRY_STR
+    );
+}
+
+static void compile_go(void) {
+    FILE *as = popen(GMCC_ASSEMBLER, "w");
+    int   c  = getc(stdin);
+
+    if (!as)
+        compile_error("failed to invoke assembler");
+
+    if (isdigit(c))
+        compile_integer(as, c - '0');
+    else if (c == '"')
+        compile_string(as);
+    else
+        compile_error("Syntax error");
+
+    fclose(as);
+    system(GMCC_LINKER);
+
+    printf("compiled, you may run with ./program\n");
 }
 
 int main() {
-    int read;
-    if (scanf("%d", &read) == EOF) {
-        fprintf(stderr, "failed to read integer\n");
-        return EXIT_FAILURE;
-    }
-
-    generate(read);
-    link();
+    compile_go();
 }
--- invoke.c
+++ invoke.c
@@ -5,9 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int gmcc_main(void);
+extern int   gmcc_entry_int(void) __attribute__((weak));
+extern char *gmcc_entry_str(void) __attribute__((weak));
 
 int main() {
-    printf("%d\n", gmcc_main());
+    if (gmcc_entry_int)
+        printf("%d\n", gmcc_entry_int());
+    else if (gmcc_entry_str)
+        printf("%s\n", gmcc_entry_str());
+    else
+        return EXIT_FAILURE;
+
     return EXIT_SUCCESS;
 }
