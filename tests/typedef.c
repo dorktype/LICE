5bb37903a43e90b7d331397996ed39f0922bf978
Dale Weiler <killfieldengine@gmail.com>
2013-10-31 13:53:08 -0400

    Show off typedef more

--- tests/typedef.c
+++ tests/typedef.c
@@ -7,8 +7,24 @@ int main(int argc, char **argv) {
     integer     a = argc;
     stringarray b = argv;
 
+    typedef struct {
+        int a;
+    } foo_t;
+
+    typedef union {
+        foo_t *data;
+    } bar_t;
+
+    foo_t foo;
+    bar_t bar;
+    foo.a = argc;
+    bar.data = &foo;
+
     expecti(a, argc);
     expecti(b, argv);
+
+    expecti(foo.a, a);
+    expecti(bar.data->a, a);
 
     return ok();
 }
